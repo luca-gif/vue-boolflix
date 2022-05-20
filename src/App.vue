0b72868cf5a32306f5ef8164c33a8e04ca91a830
luca-gif <luca.89m@gmail.com>
2022-05-20 12:17:26 +0200

    creo tutti i componenti, faccio una chiamata api e faccio partire un $emit dall'header al padre

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+
+    <header-comp @elementoCercato="valorePassato" />
+    <main-comp />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
+import HeaderComp from './components/HeaderComp.vue'
+import MainComp from './components/MainComp.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderComp,
+    MainComp
+  },
+
+  data(){
+    return{
+      moviesUrl: 'https://api.themoviedb.org/3/search/movie',
+      moviesParams: {
+        api_key: '83c397b63c322fd6a37d6c5ec3d5f6de',
+        language: 'it-IT',
+        query: 'harry potter',
+
+        filmCercato: '',
+      }
+    }
+  },
+
+  methods:{
+    apiMovies(){
+      axios.get(this.moviesUrl, {
+        params: this.moviesParams
+      } )
+      .then(r =>{
+        console.log(r.data)
+      })
+    },
+
+    valorePassato(nomeValore){
+      this.filmCercato = nomeValore;
+      console.log(this.filmCercato);
+    },
+
+  },
+
+  mounted() {
+    this.apiMovies()
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style/global';
+
+
+
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <div class="search d-flex py-4 justify-content-center">
+
+    <input v-model="ricerca" class="form-control w-25  mx-2" type="text">
+    <button @click="$emit('elementoCercato', ricerca)" class="btn btn-danger text-black">Search</button>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+  data(){
+    return{
+      ricerca: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
